8ed2adbe9b53cbe9992e4483a67b05e7950a743c
Khaled Garbaya <khaled@contentful.com>
2018-12-03 13:29:47 +0100

    feat(transformer): Add gatsby-transformer-contentful-richtext
    Co-authored-by: JP Wright <jp@contentful.com>

---
+++ packages/gatsby-transformer-contentful-richtext/src/__tests__/extend-node.js
@@ -0,0 +1,148 @@
+const {
+  graphql,
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLSchema,
+} = require(`gatsby/graphql`)
+
+const {
+  inferObjectStructureFromNodes,
+} = require(`gatsby/dist/schema/infer-graphql-type`)
+
+const { onCreateNode } = require(`../gatsby-node`)
+
+const extendNodeType = require(`../extend-node-type`)
+
+async function queryResult(nodes, fragment, { types = [] } = {}, additionalParameters) {
+  const inferredFields = inferObjectStructureFromNodes({
+    nodes,
+    types: [...types],
+  })
+  const extendNodeTypeFields = await extendNodeType(
+    {
+      type: { name: `ContentfulRichText` },
+      cache: {
+        get: () => null,
+        set: () => null,
+      },
+      getNodes: () => [],
+      ...additionalParameters,
+    },
+    {
+      renderOptions: null,
+    }
+  )
+
+  const richTextFields = {
+    ...inferredFields,
+    ...extendNodeTypeFields,
+  }
+
+  const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+      name: `RootQueryType`,
+      fields: () => {
+        return {
+          listNode: {
+            name: `LISTNODE`,
+            type: new GraphQLList(
+              new GraphQLObjectType({
+                name: `ContentfulRichText`,
+                fields: richTextFields,
+              })
+            ),
+            resolve() {
+              return nodes
+            },
+          },
+        }
+      },
+    }),
+  })
+
+  const result = await graphql(
+    schema,
+    `query {
+            listNode {
+                ${fragment}
+            }
+          }
+        `
+  )
+  return result
+}
+
+const bootstrapTest = (label, content, query, test, additionalParameters = {}) => {
+  const node = {
+    id: `whatever`,
+    children: [],
+    internal: {
+      contentDigest: `whatever`,
+      mediaType: `text/richtext`,
+    },
+  }
+  // Make some fake functions its expecting.
+  const loadNodeContent = node => Promise.resolve(node.content)
+
+  it(label, async (done) => {
+    node.content = content
+    const createNode = richTextNode => {
+      queryResult(
+        [richTextNode],
+        query,
+        {
+          types: [{ name: `ContentfulRichText` }],
+        },
+        additionalParameters
+      ).then(result => {
+        try {
+          test(result.data.listNode[0])
+          done()
+        }
+        catch(err) {
+          done.fail(err)
+        }
+      })
+    }
+    const createParentChildLink = jest.fn()
+    const actions = { createNode, createParentChildLink }
+    const createNodeId = jest.fn()
+    createNodeId.mockReturnValue(`uuid-from-gatsby`)
+    await onCreateNode({
+      node,
+      loadNodeContent,
+      actions,
+      createNodeId,
+    },
+    { ...additionalParameters }
+    )
+  })
+}
+describe(`HTML is generated correctly`, () => {
+  const content = {
+    nodeType: 'document',
+    data: {},
+    content: [
+      {
+        nodeType: 'paragraph',
+        data: {},
+        content: [
+          {
+            nodeType: 'text',
+            value: 'Hello world!',
+            marks: [],
+            data: {}
+          },
+        ],
+      },
+    ]
+  }
+  bootstrapTest(
+    `correctly loads html`,
+    content,
+    `html`,
+    (node) => {
+      expect(node.html).toEqual('<p>Hello world!</p>')
+    }
+  )
+})
---
+++ packages/gatsby-transformer-contentful-richtext/src/extend-node-type.js
@@ -0,0 +1,46 @@
+const { documentToHtmlString } = require(`@contentful/rich-text-html-renderer`)
+const { GraphQLString } = require(`gatsby/graphql`)
+
+let pathPrefixCacheStr = ``
+const htmlCacheKey = node =>
+  `transformer-contentful-rich-text-html-${
+    node.internal.contentDigest
+  }-${pathPrefixCacheStr}`
+
+module.exports = ({ type, cache, pathPrefix }, pluginOptions) => {
+  if (type.name !== `ContentfulRichText`) {
+    return {}
+  }
+
+  pathPrefixCacheStr = pathPrefix || ``
+
+  let { renderOptions } = pluginOptions
+  renderOptions = renderOptions || {}
+
+  return new Promise((resolve, reject) => {
+    async function getHTML(richTextNode) {
+      const cachedHTML = await cache.get(htmlCacheKey(richTextNode))
+
+      if (cachedHTML) {
+        return cachedHTML
+      } else {
+        const html = documentToHtmlString(
+          JSON.parse(richTextNode.internal.content),
+          renderOptions
+        )
+        // Save new HTML to cache and return
+        cache.set(htmlCacheKey(richTextNode), html)
+        return html
+      }
+    }
+    return resolve({
+      html: {
+        type: GraphQLString,
+        resolve(richTextNode) {
+          debugger
+          return getHTML(richTextNode)
+        },
+      },
+    })
+  })
+}
