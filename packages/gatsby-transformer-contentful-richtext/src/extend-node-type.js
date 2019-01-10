0228adfde3a627b2096b7f2e560f506bc51a2134
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2019-01-10 23:21:06 +0300

    feat: 🎸 add time to read for reach text

--- packages/gatsby-transformer-contentful-richtext/src/extend-node-type.js
+++ packages/gatsby-transformer-contentful-richtext/src/extend-node-type.js
@@ -2,12 +2,22 @@ const {
   documentToHtmlString
 } = require(`@contentful/rich-text-html-renderer`)
 const {
-  GraphQLString
+  documentToPlainTextString
+} = require(`@contentful/rich-text-plain-text-renderer`)
+const {
+  GraphQLString,
+  GraphQLInt
 } = require(`gatsby/graphql`)
+const words = require('lodash.words')
 
 let pathPrefixCacheStr = ``
 const htmlCacheKey = node =>
   `transformer-contentful-rich-text-html-${
+    node.internal.contentDigest
+  }-${pathPrefixCacheStr}`
+
+const ttrCacheKey = node =>
+  `transformer-contentful-rich-text-ttr-${
     node.internal.contentDigest
   }-${pathPrefixCacheStr}`
 
@@ -22,34 +32,48 @@ module.exports = ({
 
   pathPrefixCacheStr = pathPrefix || ``
 
+  async function cacheWrap(key, fn) {
+    let cached = await cache.get(key)
+
+    if (!cached) {
+      let toCache = await fn()
+      cache.set(toCache)
+
+      return toCache
+    }
+
+    return cached
+  }
+
   let {
     renderOptions
   } = pluginOptions
   renderOptions = renderOptions || {}
 
-  return new Promise((resolve, reject) => {
-    async function getHTML(richTextNode) {
-      const cachedHTML = await cache.get(htmlCacheKey(richTextNode))
-
-      if (cachedHTML) {
-        return cachedHTML
-      } else {
-        const html = documentToHtmlString(
-          JSON.parse(richTextNode.internal.content),
-          renderOptions
-        )
-        // Save new HTML to cache and return
-        cache.set(htmlCacheKey(richTextNode), html)
-        return html
-      }
-    }
+  return new Promise((resolve) => {
     return resolve({
       html: {
         type: GraphQLString,
-        resolve(richTextNode) {
-          return getHTML(richTextNode)
-        },
+        resolve: richTextNode => cacheWrap(
+          htmlCacheKey(richTextNode),
+          () => documentToHtmlString(
+            JSON.parse(richTextNode.internal.content),
+            renderOptions,
+          )),
       },
-    })
-  })
+
+      timeToRead: {
+        type: GraphQLInt,
+        resolve: richTextNode => cacheWrap(ttrCacheKey(richTextNode), () => {
+          let plainString = documentToPlainTextString(
+            JSON.parse(richTextNode.internal.content),
+          )
+          let wordsCount = words(plainString)
+          let avgWPM = 200
+
+          return Math.floor(wordsCount / avgWPM) || 1
+        }),
+      },
+    });
+  });
 }
