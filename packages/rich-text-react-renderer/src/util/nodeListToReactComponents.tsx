02e136c87d1730f11901debc05455f2a48998344
Mike Molisani <mgmolisani@gmail.com>
2019-03-11 22:41:58 -0400

    feat: 🎸 renderText option to alter all text

--- packages/rich-text-react-renderer/src/util/nodeListToReactComponents.tsx
+++ packages/rich-text-react-renderer/src/util/nodeListToReactComponents.tsx
@@ -12,14 +12,14 @@ export function nodeListToReactComponents(nodes: CommonNode[], options: Options)
 }
 
 export function nodeToReactComponent(node: CommonNode, options: Options): ReactNode {
-  const { renderNode, renderMark } = options;
+  const { renderNode, renderMark, renderText } = options;
   if (helpers.isText(node)) {
     return node.marks.reduce((value: ReactNode, mark: Mark): ReactNode => {
       if (!renderMark[mark.type]) {
         return value;
       }
       return renderMark[mark.type](value);
-    }, node.value);
+    }, renderText ? renderText(node.value) : node.value);
   } else {
     const children: ReactNode = nodeListToReactComponents(node.content, options);
     if (!node.nodeType || !renderNode[node.nodeType]) {
