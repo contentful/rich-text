f038e384fbf6dc9ef932f89da5c94628023d0519
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2024-05-22 10:06:41 +0200

    chore: upgrading rollup to latest version []

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import json from 'rollup-plugin-json';
 import sourceMaps from 'rollup-plugin-sourcemaps';
 import typescript from 'rollup-plugin-typescript2';
-import json from 'rollup-plugin-json';
 
 export default (outputFile, overrides = {}) => ({
   input: 'src/index.ts',
