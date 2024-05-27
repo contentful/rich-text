c43938fbf5e2ad574ae9f21a9824c05eb84c46a2
Yves Rijckaert <hi@yvesrijckaert.com>
2024-05-27 13:56:11 +0200

    chore: revert "feat: upgrading rollup to latest version [TOL-2097] (#559)"
    This reverts commit f14d1974590d58f92bbf4cb56644095dba929ad9.

--- packages/contentful-slatejs-adapter/rollup.config.ts
+++ packages/contentful-slatejs-adapter/rollup.config.ts
@@ -1,7 +1,8 @@
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import json from 'rollup-plugin-json';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import sourceMaps from 'rollup-plugin-sourcemaps';
 import typescript from 'rollup-plugin-typescript2';
+import json from 'rollup-plugin-json';
 
 const pkg = require('./package.json');
 
@@ -24,6 +25,9 @@ const options = {
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
+
+    // Resolve source maps to the original source
+    sourceMaps(),
   ],
 };
 
