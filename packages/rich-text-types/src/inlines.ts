12feb8381de475eafa34889ce5c02a75337060b3
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2024-07-17 14:58:04 +0200

    feat: update inline enum order

--- packages/rich-text-types/src/inlines.ts
+++ packages/rich-text-types/src/inlines.ts
@@ -2,10 +2,10 @@
  * Map of all Contentful inline types. Inline contain inline or text nodes.
  */
 export enum INLINES {
-  HYPERLINK = 'hyperlink',
-  ENTRY_HYPERLINK = 'entry-hyperlink',
   ASSET_HYPERLINK = 'asset-hyperlink',
-  RESOURCE_HYPERLINK = 'resource-hyperlink',
   EMBEDDED_ENTRY = 'embedded-entry-inline',
   EMBEDDED_RESOURCE = 'embedded-resource-inline',
+  ENTRY_HYPERLINK = 'entry-hyperlink',
+  HYPERLINK = 'hyperlink',
+  RESOURCE_HYPERLINK = 'resource-hyperlink',
 }
