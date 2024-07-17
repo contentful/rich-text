8643e1126ce765842190a866556d7c0fa45a7ba8
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2024-07-17 15:14:28 +0200

    feat: update inline enum order [] (#620)
    * feat: update schema for rich text types
    
    * chore: update snapshots
    
    * chore: update schema
    
    * chore: update snapshots
    
    * chore: update table cell nodetype
    
    * chore: refactor row & cell types
    
    * feat: update inline enum order
    
    * chore: add note for inline type

--- packages/rich-text-types/src/inlines.ts
+++ packages/rich-text-types/src/inlines.ts
@@ -1,11 +1,15 @@
 /**
  * Map of all Contentful inline types. Inline contain inline or text nodes.
+ *
+ * @note This should be kept in alphabetical order since the
+ * [validation package](https://github.com/contentful/content-stack/tree/master/packages/validation)
+ *  relies on the values being in a predictable order.
  */
 export enum INLINES {
-  HYPERLINK = 'hyperlink',
-  ENTRY_HYPERLINK = 'entry-hyperlink',
   ASSET_HYPERLINK = 'asset-hyperlink',
-  RESOURCE_HYPERLINK = 'resource-hyperlink',
   EMBEDDED_ENTRY = 'embedded-entry-inline',
   EMBEDDED_RESOURCE = 'embedded-resource-inline',
+  ENTRY_HYPERLINK = 'entry-hyperlink',
+  HYPERLINK = 'hyperlink',
+  RESOURCE_HYPERLINK = 'resource-hyperlink',
 }
