6781dc16e8ba66fc628c36195f9860257c3e452d
Chris Helgert <chrishelgert@users.noreply.github.com>
2024-10-28 16:45:34 +0100

    refactor(build): exclude dependencies from bundle []

--- packages/rich-text-types/rollup.config.js
+++ packages/rich-text-types/rollup.config.js
@@ -1,32 +1,6 @@
-import copy from 'rollup-plugin-copy';
-
 import config from '../../rollup.config';
-import { main as outputFile } from './package.json';
+import { main as outputFile, dependencies } from './package.json';
 
-const customConfig = () => {
-  const baseConfig = config(outputFile);
-
-  return {
-    ...baseConfig,
-    output: {
-      // Maintain the same file names & folders structure as "src"
-      // This is necessary to resolve generated JSON schema files
-      //
-      // https://rollupjs.org/guide/en/#outputpreservemodules
-      preserveModules: true,
-    },
-    plugins: [
-      ...baseConfig.plugins,
-      copy({
-        targets: [
-          {
-            src: 'src/schemas/generated',
-            dest: 'dist/schemas',
-          },
-        ],
-      }),
-    ],
-  };
-};
-
-export default customConfig();
+export default config(outputFile, {
+  external: Object.keys(dependencies),
+});
